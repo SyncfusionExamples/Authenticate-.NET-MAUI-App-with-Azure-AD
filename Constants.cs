9acd366c9928175229a99913b7c4ee72758b7638
Paul Anderson <42271912+PaulAndersonS@users.noreply.github.com>
2023-08-25 17:49:00 +0530

    Update Constants.cs

--- Constants.cs
+++ Constants.cs
@@ -3,7 +3,6 @@
 public static class Constants
 {
     //The Application or Client ID will be generated while registering the app in the Azure portal. Copy and paste the GUID.
-    //Client_ID=3bf5e165-a671-44e9-a412-22f13c737078
     public static readonly string ClientId = "Client_ID";
 
     //Leaving the scope to its default values
